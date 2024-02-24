c9bb05a145ceb2a9e11ff9d34c8ad775bc00c586
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-02-24 20:26:23 +0000

    Borrado

--- src/app/core/interfaces/survey-form.i.ts
+++
@@ -1,7 +0,0 @@
-export interface MenoresEdad {
-    edad: number | string | null;
-    am: string | null;
-    sexo: number | string | null;
-}
-
-export type nameValue = 'menoresDeEdad' | 'sexo' | 'codigo' | 'fechaEncuesta' | 'fechaIngresoPais' | 'nombre' | 'edad' | 'embarazo';

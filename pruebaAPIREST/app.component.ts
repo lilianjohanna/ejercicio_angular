e28dac9fb0b8f5b902515e1f821e851d8ca20e33
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-02-24 03:24:31 +0000

    Cambios 4.2

--- pruebaAPIREST/app.component.ts
+++ pruebaAPIREST/app.component.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: "./app.component.html",
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {

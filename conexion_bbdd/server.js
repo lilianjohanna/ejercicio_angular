3ddf6720863b5935059ea910af36ea43cd34b2d4
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-02-24 23:04:50 +0000

    Nuevo proyecto 2.4

--- conexion_bbdd/server.js
+++ conexion_bbdd/server.js
@@ -30,7 +30,7 @@ app.get('/datos-select', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3306;
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });

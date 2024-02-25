d249f64cf80f581006346295bdee587f22140ed1
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-02-25 20:48:56 +0000

    ...

--- conexion_angularmysql/src/app/select-datos/select-datos.component.ts
+++ conexion_angularmysql/src/app/select-datos/select-datos.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-select-datos',
@@ -7,6 +8,31 @@ import { Component } from '@angular/core';
   templateUrl: './select-datos.component.html',
   styleUrl: './select-datos.component.css'
 })
-export class SelectDatosComponent {
+export class FormExampleComponent implements OnInit {
 
+  // Array para almacenar los resultados de la consulta a la base de datos
+  options: any[] = [];
+
+  // Variable para almacenar el valor seleccionado del select
+  selectedOption: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    // Hacer la solicitud HTTP para obtener datos de la base de datos
+    this.http.get<any[]>('http://tu-api.com/obtener-datos-de-la-base-de-datos')
+      .subscribe(data => {
+        this.options = data; // Asignar los datos obtenidos al array options
+      });
+  }
+
+  // Método para manejar la selección de una opción en el select
+  onSelect(option: any): void {
+    this.selectedOption = option;
+  }
+
+  // Método para enviar el formulario (puedes agregar más lógica según sea necesario)
+  onSubmit(): void {
+    console.log('Formulario enviado:', this.selectedOption);
+  }
 }

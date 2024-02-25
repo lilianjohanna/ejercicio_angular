a7dbf8dd5904911e8d383ba5044e0279769421eb
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-02-25 19:45:49 +0000

    Proyecto en blanco 1.2

---
+++ conexion_angular_mysql/src/app/select-datos/httpclientmodule.ts
@@ -0,0 +1,14 @@
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    // Componentes
+  ],
+  imports: [
+    // Otros módulos
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

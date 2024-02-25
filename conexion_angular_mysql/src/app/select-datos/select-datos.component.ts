89a9194af29ff8d17f2bbfd957a75ab5f03a8572
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-02-25 20:07:37 +0000

    ...

---
+++ conexion_angular_mysql/src/app/select-datos/select-datos.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-tu-componente',
+  templateUrl: './select-datos.component.html',
+  styleUrls: ['./select-datos.component.css']
+})
+export class TuComponenteComponent implements OnInit {
+
+  opciones: any[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.cargarOpciones();
+  }
+
+  cargarOpciones() {
+    this.http.get<any[]>('http://localhost:3000/opciones').subscribe(
+      data => {
+        this.opciones = data;
+      },
+      error => {
+        console.log('Error al cargar opciones desde el servidor', error);
+      }
+    );
+  }
+
+}

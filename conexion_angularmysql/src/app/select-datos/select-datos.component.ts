9acc5d2847413de9400246b29c34a56bac282991
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-02-25 20:46:46 +0000

    ...

---
+++ conexion_angularmysql/src/app/select-datos/select-datos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-select-datos',
+  standalone: true,
+  imports: [],
+  templateUrl: './select-datos.component.html',
+  styleUrl: './select-datos.component.css'
+})
+export class SelectDatosComponent {
+
+}

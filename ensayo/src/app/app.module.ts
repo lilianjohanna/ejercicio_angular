48bebfc1555c8cf4244a1198613eb6da3dac551c
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-02-25 21:31:07 +0000

    ...

---
+++ ensayo/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
+import { FormNewItemComponent } from './form-new-item/form-new-item.component';
+import { CitiesComponent } from './cities/cities.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FilterPipe } from './pipes/filter.pipe';
+import { ContactComponent } from './contact/contact.component';
+import { ContactReactiveComponent } from './contact-reactive/contact-reactive.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ButtonComponent,
+    FormNewItemComponent,
+    CitiesComponent,
+    FilterPipe,
+    ContactComponent,
+    ContactReactiveComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

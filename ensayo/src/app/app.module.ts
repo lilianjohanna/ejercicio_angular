ee2fad4c2dddb846adf05ea05581809283a374aa
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-02-25 22:17:27 +0000

    ...

--- ensayo/src/app/app.module.ts
+++ ensayo/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { bootstrapApplication } from '@angular/platform-browser';
 import { SelectivoComponent } from './selectivo/selectivo.component'; // Importa tu componente aquí
 
 @NgModule({

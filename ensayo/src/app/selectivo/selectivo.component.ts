a81ce12c191f96a4f74e3d222650f6f247a15365
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-02-25 21:48:43 +0000

    ...

--- ensayo/src/app/selectivo/selectivo.component.ts
+++ ensayo/src/app/selectivo/selectivo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-selectivo',
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './selectivo.component.css'
 })
 export class SelectivoComponent {
-
+  @Input() label: string = '';
+    @Input() icon: string = '';
+    @Input() type: string = 'text';
+    @Input() index: string = '';
+    @Input() ui: 'default' | 'lg' | 'sm' = 'lg';
+    @Input() options: any[] = [];
+    get sizeLabel() {
+        switch(this.ui) {
+        case 'lg':
+            return 'mb-2 text-[1.1rem] font-semibold';
+        default:
+            return 'mb-1 text-[.9rem]'
+        }
+    }
 }

46db026a8e785d27e30c70e6b9ead522f75d84af
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-02-26 02:15:56 +0000

    ...

---
+++ ensayo/src/app/app.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+
+  getOptions(): Observable<any[]> {
+    return this.http.get<any[]>('http://localhost:3000/options'); // Ruta a tu servidor Node.js
+  }
+}

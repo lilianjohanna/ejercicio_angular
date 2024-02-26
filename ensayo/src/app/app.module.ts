ef0d5619bb19c909575f7d1ac8ba9be7235be7f5
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-02-26 02:42:37 +0000

    ...

--- ensayo/src/app/app.module.ts
+++ ensayo/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SelectivoComponent } from './selectivo/selectivo.component'; // Importa tu componente aquí
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    SelectivoComponent // Agrega tu componente a la lista de declaraciones
+    SelectivoComponent, // Agrega tu componente a la lista de declaraciones
+    HttpClientModule
   ],
   imports: [
     BrowserModule

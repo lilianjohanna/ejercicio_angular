2f088978e9a055085ef79077ca3112a393539adb
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-03-01 16:44:09 +0000

    ...

---
+++ src/app/components/shared/nav-top/nav-top.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-nav-top',
+    standalone: true,
+    imports: [],
+    templateUrl: './nav-top.component.html'
+})
+export class NavTopComponent {
+
+}
